24ba1ef13af57248771daafd6a7f11d72258a1ea
Hiroomi Okoshi <hiroomi@okoshi.net>
2016-09-02 18:28:12 +0900

    ファイルを追加

---
+++ ArrayWithForEach/ArrayWithForEach.swift
@@ -0,0 +1,18 @@
+//
+//  ArrayWithForEach.swift
+//  ArrayWithForEach
+//
+//  Created by 小越広臣 on 2016/09/02.
+//  Copyright © 2016年 Hiroomi Okoshi. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    func forEach(consumer: (Element) -> Void) -> Void {
+        for obj in self {
+            consumer(obj as Element)
+        }
+    }
+}
