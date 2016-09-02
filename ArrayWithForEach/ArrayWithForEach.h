711841c12b5102d0660a852d647f110848e151d3
okosheep <hiroomi+github@okoshi.net>
2016-09-02 16:55:43 +0900

    Initial Commit

---
+++ ArrayWithForEach/ArrayWithForEach.h
@@ -0,0 +1,19 @@
+//
+//  ArrayWithForEach.h
+//  ArrayWithForEach
+//
+//  Created by 小越広臣 on 2016/09/02.
+//  Copyright © 2016年 Hiroomi Okoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArrayWithForEach.
+FOUNDATION_EXPORT double ArrayWithForEachVersionNumber;
+
+//! Project version string for ArrayWithForEach.
+FOUNDATION_EXPORT const unsigned char ArrayWithForEachVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArrayWithForEach/PublicHeader.h>
+
+
